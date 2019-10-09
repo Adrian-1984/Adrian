d5f763dcc815ca9572362a7174e4bc669eb7d305
adre <84japko@gmail.com>
2019-10-09 10:50:04 +0200

    added sum digits

--- HelloWorldClass.java
+++ HelloWorldClass.java
@@ -16,5 +16,13 @@ public class HelloWorldClass {
 	public static void main(String[] args) {
 		HelloWorldClass hwc = new HelloWorldClass("Conrtuctor parameter");
 		System.out.println(hwc.greet());
+		int number = 35;
+		int sumDigit = 0;
+		while (number > 0) {
+			sumDigit = sumDigit + number % 10; // 0 + 35%10 = 5
+			number = number /10;		   // 35/10 = 3.5 = 3 rounded
+		}
+		System.out.println(sumDigit);
+
 	}
 }
